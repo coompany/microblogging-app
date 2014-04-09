2a7f00db2b096d4d80bf2b9b12f530711bc450d7
Andrea Jemmett <andreajemmett@gmail.com>
2014-04-09 21:14:43 +0200

    Fixed error on watch task -> added bootstrap rel tasks

--- tasks/register/syncAssets.js
+++ tasks/register/syncAssets.js
@@ -1,7 +1,9 @@
 module.exports = function (grunt) {
 	grunt.registerTask('syncAssets', [
 		'jst:dev',
+    'copy:bootstrap',
 		'less:dev',
+    'clean:bootstrap',
 		'sync:dev',
 		'coffee:dev'
 	]);
